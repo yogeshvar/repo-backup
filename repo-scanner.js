e046beeb0ca1df5cf5db2b0992704d45b0d8a15c
Yogeshvar <yogeshvar@icloud.com>
2020-03-02 10:33:19 +0530

    :tada: Initial commit

---
+++ repo-scanner.js
@@ -0,0 +1,86 @@
+const { lstatSync, readdirSync, existsSync } = require('fs');
+const { join } = require('path');
+const os = require('os');
+const { exec } = require('child_process');
+
+const isFile = source => lstatSync(source).isFile();
+
+const isDirectory = source => lstatSync(source).isDirectory();
+
+const getDirectories = source =>
+  readdirSync(source)
+    .map(name => join(source, name))
+    .filter(isDirectory);
+
+const isDotDirectory = path => /(^|\/)\.[^\/\.]/g.test(path);
+
+const getDirectoryNames = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(directory => directory.isDirectory())
+    .map(directory => directory.name);
+
+const getFiles = source =>
+  readdirSync(source)
+    .map(name => join(source, name))
+    .filter(isFile);
+
+const ignoreDotDirectories = source =>
+  readdirSync(source)
+    .map(name => join(source, name))
+    .filter(file => {
+      if (isDotDirectory(file)) {
+        return null;
+      } else {
+        return file;
+      }
+    });
+const isGitRepo = directory => existsSync(join(directory, '.git'));
+
+getRootDirectories = ignoreDotDirectories(os.homedir());
+
+let gitRepos = [];
+
+getRootDirectories.forEach(rootDir => {
+  if (!isDirectory(rootDir)) {
+    if (isGitRepo(rootDir)) {
+      gitRepos.push(rootDir);
+    }
+  } else {
+    const x = ignoreDotDirectories(rootDir);
+    x.forEach(dir => {
+      if (isGitRepo(dir)) {
+        gitRepos.push(dir);
+      } else if (isDirectory(dir)) {
+        const y = getDirectories(dir);
+        y.forEach(directory => {
+          if (isGitRepo(directory)) {
+            gitRepos.push(directory);
+          } else if (isDirectory(directory)) {
+            const z = getDirectories(directory);
+            z.forEach(dir => {
+              if (isGitRepo(dir)) {
+                gitRepos.push(dir);
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+gitRepos.forEach(gitRepo => {
+  exec(
+    `cd ${gitRepo} && git config --get remote.origin.url`,
+    (err, stdout, stderr) => {
+      if (err) {
+        //some err occurred
+        console.error(err);
+      } else {
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      }
+    }
+  );
+});
